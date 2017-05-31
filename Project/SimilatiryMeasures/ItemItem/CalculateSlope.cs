76346a14658e75294b564da0f4ce807dc79b2691
Bram M <bremsterr@gmail.com>
2017-05-31 22:20:34 +0200

    Implemented matrix for deviation calculations

--- Project/SimilatiryMeasures/ItemItem/CalculateSlope.cs
+++ Project/SimilatiryMeasures/ItemItem/CalculateSlope.cs
@@ -5,14 +5,19 @@ namespace SimilatiryMeasures.ItemItem
 {
     class CalculateSlope
     {
+        //TODO Improve speed
+        //TODO implement matrix
         public double ProcessData(Dictionary<int, Dictionary<int, double>> ratings, int itemId1, int itemId2)
         {
             var filteredRatings = ratings.Where(x => x.Value.ContainsKey(itemId1) && x.Value.ContainsKey(itemId2)).ToDictionary(x => x.Key, x => x.Value);
             //var filteredRatings = ratings.Where(x => x.Value.ContainsKey(itemId1) && x.Value.ContainsKey(itemId2)).Select(x => new UserRatings { UserId = x.Key, Ratings = x.Value}).ToList();
 
-            var devidation = CalculateDeviation(filteredRatings, itemId1, itemId2);
+            if (!filteredRatings.Any())
+                return 0.0;
 
-            return devidation;
+            var deviation = CalculateDeviation(filteredRatings, itemId1, itemId2);
+
+            return deviation;
         }
 
         public double CalculateDeviation(Dictionary<int, Dictionary<int, double>> userRatings, int itemId1, int itemId2)
--- Project/SimilatiryMeasures/Program.cs
+++ Project/SimilatiryMeasures/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using SimilatiryMeasures.ItemItem;
 using SimilatiryMeasures.UserItem;
 
@@ -11,16 +14,22 @@ namespace SimilatiryMeasures
             //TODO update item-rating calculation and similarRatings
             var dictionary = CsvReader.ReadConnections();
 
-            var dataX = new[] {5.0, 1.0, 3.0};
-            var dataY = new[] {2.0, 4.0, 3.0};
+            //RunUserItemMethods(dictionary);
+            RunItemItemMethods(dictionary);
+        }
+
+        private static void RunUserItemMethods(Dictionary<int, Dictionary<int, double>> dictionary)
+        {
+            var dataX = new[] { 5.0, 1.0, 3.0 };
+            var dataY = new[] { 2.0, 4.0, 3.0 };
 
             var eDis = SimilarityCalculations.CalculateEculeanDistanceCoefficient(dataX, dataY);
             var mDis = SimilarityCalculations.CalculateManhattanDistanceCoefficient(dataX, dataY);
             var r = SimilarityCalculations.CalculatePearsonCoefficient(dataX, dataY);
 
             //Take into account values that haven't been rated by both users (which I skipped in the previous calculations (here 0.0))
-            var dataCosX = new[] {5.0, 0.0 ,1.0, 3.0, 5.0};
-            var dataCosY = new[] {2.0, 5.0, 4.0, 3.0, 0.0};
+            var dataCosX = new[] { 5.0, 0.0, 1.0, 3.0, 5.0 };
+            var dataCosY = new[] { 2.0, 5.0, 4.0, 3.0, 0.0 };
 
             var cosSim = SimilarityCalculations.CalculateCosineSimilarityCoefficient(dataCosX, dataCosY);
 
@@ -29,15 +38,66 @@ namespace SimilatiryMeasures
 
             foreach (var item in nearestNeigbours)
             {
-                Console.WriteLine("Id: {0}  Similarity: {1}" ,item.Key, item.Similarity);
+                Console.WriteLine("Id: {0}  Similarity: {1}", item.Key, item.Similarity);
             }
 
             //PredictedRatingCalculations predictedRatingCalculations = new PredictedRatingCalculations();
             //var predRating = predictedRatingCalculations.CalculatePredictedRating(101, nearestNeigbours, dictionary);
             //Console.WriteLine("Predicted Rating: {0}" , predRating);
+        }
+
+        //TODO skip m == k
+        //TODO 
+        private static void RunItemItemMethods(Dictionary<int, Dictionary<int, double>> dictionary)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
 
             CalculateSlope calculateSlope = new CalculateSlope();
-            calculateSlope.ProcessData(dictionary, 101, 102);
+
+            var movieIds = dictionary.Values.Select(x => x.Keys).ToArray();
+            var keysets = new List<int>();
+
+            for (int j = 0; j < movieIds.Length; j++)
+            {
+                var id = movieIds[j].ToArray();
+                for (int i = 0; i < id.Length; i++)
+                {
+                    if (!keysets.Contains(id[i]))
+                    {
+                        keysets.Add(id[i]);
+                    }
+                }
+            }
+            var orderedKeysets = keysets.OrderBy(x => x).ToArray();
+
+            var deviationsMatrix = new double[orderedKeysets.Length, orderedKeysets.Length];
+
+            for (int m = 0; m < orderedKeysets.Length; m++)
+            {
+                var kIndex = m;
+                while (kIndex < orderedKeysets.Length)
+                {
+                    var deviation = calculateSlope.ProcessData(dictionary, orderedKeysets[m], orderedKeysets[kIndex]);
+                    deviationsMatrix[m, kIndex] = deviation;
+                    //Invert calculated deviation (positive to negative and vise versa)
+                    deviationsMatrix[kIndex, m] = deviation * -1;
+
+                    kIndex++;
+                }
+            }
+
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+
         }
     }
 }
